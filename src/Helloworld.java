523e8fb8c6067e5c4d8bb6ffba3ce6d686cce8c2
Tobias Nedbal <tobias.nedbal@infis.cz>
2024-09-12 15:09:03 +0200

    putin chočet mir

---
+++ src/Helloworld.java
@@ -0,0 +1,44 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+        System.out.println("Zdravstvuj drug");
+    }
+}
